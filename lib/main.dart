745c89ae542da401f4441633ffface44fcf7cd36
Arnab Mondal <arnabmondal2509@gmail.com>
2025-06-09 19:10:19 +0530

    Implement Firebase initialization and add Firebase options configuration for web platform

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:portfolio_web/app/routes/app_pages.dart';
 import 'package:portfolio_web/utils/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  // WidgetsFlutterBinding.ensureInitialized();
   // Firebase initialization commented out temporarily
-  // await Firebase.initializeApp(
-  //   options: const FirebaseOptions(
-  //     apiKey: "YOUR_API_KEY",
-  //     authDomain: "YOUR_AUTH_DOMAIN",
-  //     projectId: "YOUR_PROJECT_ID",
-  //     storageBucket: "YOUR_STORAGE_BUCKET",
-  //     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  //     appId: "YOUR_APP_ID",
-  //     measurementId: "YOUR_MEASUREMENT_ID",
-  //   ),
-  // );
+ await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+
+  );
   
   // Ensure old instances are cleared before starting app
   await Get.deleteAll(force: true);
